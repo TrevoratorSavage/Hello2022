88111a4dd1795ce32266fe8d14436aed1e5b9f00
zzzBananamanzzz <114937133+zzzBananamanzzz@users.noreply.github.com>
2022-10-03 12:21:32 -0400

    Update HelloMain.java

--- src/HelloMain.java
+++ src/HelloMain.java
@@ -15,7 +15,7 @@ public class HelloMain {
 
     /**
      *  this is our main method
-     * @param args the command line argument
+     * @param args the command line arguments
      */
     public static void main(String[]args){
 
