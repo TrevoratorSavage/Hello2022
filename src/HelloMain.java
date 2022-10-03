9d67b1c3b64ee7a1ba2d1f50c3381b4d3eb8318c
zzzBananamanzzz <trevoratorsavage1@gmail.com>
2022-10-03 12:14:01 -0400

    First commit

---
+++ src/HelloMain.java
@@ -0,0 +1,35 @@
+//this is *totally* our first code
+import java.util.*;
+
+/**
+ * This is the HelloMain class for the demo
+ * Trevor Savage and Calvin Besch
+ */
+public class HelloMain {
+
+    /**
+     * This is a max number
+     */
+    public static final int MAX_NUM = 10;
+
+
+    /**
+     *  this is our main method
+     * @param args the command line argument
+     */
+    public static void main(String[]args){
+
+        /*
+        this is the first command
+         */
+
+        System.out.println("Hello World!");
+
+        /*
+        this is the second command
+         */
+        int firstNum = 3;
+        System.out.println("The number " + firstNum + " is less than " + MAX_NUM);
+
+    }
+}
